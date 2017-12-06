d1cf7216dc89df500a47d3b74444c2a3a2e5eb47
Anubhav Gupta <anubhav.ankit87@gmail.com>
2017-12-07 00:02:07 +0530

    Sample App with Library

---
+++ app/src/main/java/com/example/android/customdialogbox/SharedPref.java
@@ -0,0 +1,12 @@
+package com.example.android.customdialogbox;
+
+/**
+ * Created by John on 7/11/17.
+ */
+
+public class SharedPref {
+    public final static String  PREFERENCE_NAME       = "PREF_SAMPLE_PROJECT";
+
+    public final static boolean FIRST_WELCOME_DEFAULT = false;
+    public final static String  KEY_FIRST_WELCOME     = "firstWelcome";
+}
